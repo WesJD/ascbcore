818a23d7d5959d080ec51790de360b0eb8389558
glorantq <glorantq@gmail.com>
2016-06-30 19:45:18 +0200

    Added abstract arena code

--- src/main/java/ascb/nivk/core/arena/Arena.java
+++ src/main/java/ascb/nivk/core/arena/Arena.java
@@ -1,7 +1,16 @@
 package ascb.nivk.core.arena;
 
+import ascb.nivk.core.SCBPlayer;
+import org.bukkit.Location;
+
+import java.util.List;
+
 /**
  * Created by Nivk on 2016. 06. 30..
  */
-public class Arena {
+public abstract class Arena {
+    public abstract String getName();
+    public abstract List<SCBPlayer> getPlayers();
+    public abstract List<Location> getSpawnpoints();
+    public abstract Location getLobbyLocation();
 }
